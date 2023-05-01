134947e9b9d7ae43e47c1fe7e68a06dfbda23f1a
Luke <matthews.luke.c@gmail.com>
2023-05-01 11:57:46 +1000

    Forgot to checkin the new BoatService

---
+++ src/main/java/com/sailingwebtools/marina/service/BoatService.java
@@ -0,0 +1,32 @@
+package com.sailingwebtools.marina.service;
+
+import com.sailingwebtools.marina.model.Boat;
+import com.sailingwebtools.marina.repository.BoatRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class BoatService {
+    @Autowired
+    private BoatRepository boatRepository;
+
+    public Page<Boat> getAllBoats(Pageable page) {
+        log.info(page.toString());
+        return boatRepository.findAll(Example.of(Boat.builder().archived(false).build()), page);
+    }
+
+    public List<Boat> findBoats(String search) {
+        return boatRepository.findBoatByNameSailNumberContact(search == null ? "" : search.toUpperCase());
+    }
+
+    public Page<Boat> findBoats(String search, Pageable pageable) {
+        return boatRepository.findBySearchTerm(search, pageable);
+    }
+}
