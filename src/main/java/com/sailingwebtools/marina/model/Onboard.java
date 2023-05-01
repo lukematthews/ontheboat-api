822bfd914c46adbb1402c0c9f116790434375b49
Luke <matthews.luke.c@gmail.com>
2023-05-02 07:44:25 +1000

    Fixed mapping

--- src/main/java/com/sailingwebtools/marina/model/Onboard.java
+++ src/main/java/com/sailingwebtools/marina/model/Onboard.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,10 +17,10 @@ public class Onboard {
     @Id
     @GeneratedValue
     private Long id;
-    
-    @Column
+
+    @ManyToOne
     private Boat boat;
-    @Column
+    @ManyToOne
     private Crew crew;
     @Column
     private LocalDateTime timeOn;
