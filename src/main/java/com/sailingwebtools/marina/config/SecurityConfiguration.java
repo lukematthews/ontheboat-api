a59a696c824b2eb54578bc116de2b624106d18f7
Luke <matthews.luke.c@gmail.com>
2023-05-16 12:40:22 +1000

    Security configuration that allows everything still for the time being.

---
+++ src/main/java/com/sailingwebtools/marina/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.sailingwebtools.marina.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/**").permitAll()
+                        .requestMatchers(toH2Console()).permitAll()
+                        .anyRequest().authenticated()
+                );
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
+    }
+}
