6287351451664f1b7d08b48e140dc396ea4ea3a5
Luke <matthews.luke.c@gmail.com>
2023-08-12 06:52:11 +1000

    Allow for deleting of media in the repository

--- src/main/java/au/com/ontheboat/api/repository/BoatMediaRepository.java
+++ src/main/java/au/com/ontheboat/api/repository/BoatMediaRepository.java
@@ -3,6 +3,11 @@ package au.com.ontheboat.api.repository;
 
 import au.com.ontheboat.api.model.BoatMedia;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BoatMediaRepository extends JpaRepository<BoatMedia, Long> {
+    @Modifying
+    @Query
+    void deleteByFileId(String fileId);
 }
